124f3395fde23e7b5ecb21940afc650aba3122e1
Gabrielo <espinola.gabriel@gmail.com>
2018-07-31 23:30:11 -0500

    Update coin-flip.js
    Optimized a bit and added a few comments

--- coin-flip.js
+++ coin-flip.js
@@ -3,27 +3,29 @@ export default function randomNumber(n) {
     Your implementation, using the flip() function
   */
  var nInt = parseInt(n,10);
+ //edge case handle
  if(nInt<1 || nInt>=1000000){
      throw new Error ("n is out of range, n must be greater than 0 and less than 1000000");
  }
   
   var bin = nInt.toString(2);
-  var randNum = '';
-  
+  var newRand = 0;
   for(var i=1; i<=bin.length;i++){
-      randNum = randNum+Number(flip());
-      let currentRandNum = parseInt(randNum,2);
-          currentRandNum = currentRandNum <<bin.length-i;//bin.length-i
-
-      if(currentRandNum>=nInt){
-          currentRandNum = currentRandNum >>(bin.length-i)+1; 
-          currentRandNum = currentRandNum <<1;
-          randNum = currentRandNum.toString(2);
+      newRand = newRand << 1;
+      newRand = newRand | Number(flip());
+      newRand = newRand <<bin.length-i;
+      if(newRand>=nInt){
+          //this is how I handle the number generated being greter than n, replace last bit with a 0 so it will be lesser
+          newRand = newRand >>(bin.length-i)+1; 
+          newRand = newRand <<1;
+      }
+      else{
+          newRand = newRand >>(bin.length-i);
       }
       
   }
   
-  return parseInt(randNum,2);
+  return newRand;
 }
 
 function flip() {
